0532d7a0c1287d67e939b31533264f3007030ecc
なつこ <m-toda@za.biglobe.co.jp>
2018-02-05 14:02:21 +0900

    rack-corsを追加

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,11 @@ module TodoApiRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :delete]
+      end
+    end
   end
 end
