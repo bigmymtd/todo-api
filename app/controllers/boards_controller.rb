989645783bfd6a7cbb75bd6f5eb071e6a7840fde
なつこ <m-toda@za.biglobe.co.jp>
2018-02-05 14:22:14 +0900

    :construction:

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,3 +1,21 @@
 class BoardsController < ApplicationController
+  def show
+    id = params[:id].presence || 1
+    render json: {
+        status: true
+    }
+  end
 
+  def index
+    render json: {
+        status: 200,
+        result: ['aa', 'bb']
+    }
+  end
+
+  def creat
+    render json: {
+        status: true
+    }
+  end
 end
