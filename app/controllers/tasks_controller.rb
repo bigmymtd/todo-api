f17bb1d266e1c0dfef364826a8c68e227d3d99b0
なつこ <m-toda@za.biglobe.co.jp>
2018-02-22 16:59:27 +0900

    ボードにタスクを新規追加するAPIを追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @task = Task.new(name: params[:name], status: 1)
+    if @task.save
+      @board = Board.find_by(id: params[:board_id])
+      @board.task_list = @board.task_list + "#{@board.task_list.present? ? ',' : ''}#{@task.id}"
+
+      if @board.save
+        render json: {
+          status: 200,
+          result: {
+            board: @board,
+            task: @task
+          }
+        }
+      else
+        render json: 500
+      end
+    else
+      render json: 500
+    end
+  end
+end
